55eb60a59560ba4e447d3bdfcd4e924e458137ca
1GLEBOS1 <glebxx04@gmail.com>
2023-06-06 19:36:11 +0300

    Add project files.

---
+++ Authentificator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Authentificator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
